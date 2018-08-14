942b62aeb9b6fd88a69baad28c82a86f7035f5e7
godzhang <zhangqijilindaxue@126.com>
2018-08-14 11:39:11 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home-page">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  async created (){
+    await this.init()
+    console.log(1)
+  },
+  methods: {
+    init () {}
+  },
+  components: {
+
+  }
+}
+</script>
